5dd6a4f53446169dfd27ec86f6fb48ec262f3f37
Masayuki Kimura <lig.kimura@gmail.com>
2017-03-23 07:01:39 +0000

    課題の提出前

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
         return redirect_to root_url if @micropost.nil?
         @micropost.destroy
         flash[:success] = "Micropost deleted"
-        redirect_to request.referrer || root_ur
+        redirect_to request.referrer || root_url
     end
     
     private
