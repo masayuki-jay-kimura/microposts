39eb6d7f2c32111b69b480f9fef398ce2cb5e034
Masayuki Kimura <lig.kimura@gmail.com>
2017-04-01 14:19:13 +0000

    提出用に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
+    @microposts_pages = @microposts.all.page(params[:page])
   end
   
   def new
