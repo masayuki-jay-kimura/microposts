ed8467eece76d5213fc98e841526a004cd9a4ed3
Masayuki Kimura <lig.kimura@gmail.com>
2017-04-02 11:40:32 +0000

    @feed_items_pagesの削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-      @feed_items_pages = @feed_items.all.page(params[:page])
     end
   end
 end
